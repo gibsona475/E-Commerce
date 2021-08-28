4fa6e505047b177d7514886235c772fc89f66257
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-08-27 18:17:04 -0700

    Added async to try/catch in routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
 
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -36,7 +36,7 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   try {
     const productData = await Product.create(req.body);
     res.status(200).json(productData);
